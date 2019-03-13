ea1702a20e1e906252d49013aab1bba7c83dbf5b
patrycjawolf@hotmail.com <patrycjawolf@hotmail.com>
2019-03-13 19:44:10 +0100

    gg

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
 namespace Hello
 {
@@ -10,6 +7,7 @@ namespace Hello
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Witaj Świecie!");
         }
     }
 }
